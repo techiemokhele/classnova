509ca67e086121d76a5db02c163cead7f6d82f19
techiemokhele <neomokhele23@gmail.com>
2024-07-04 05:04:59 +0200

    Implement rating modal

--- app/(root)/resources/people-reviews/page.tsx
+++ app/(root)/resources/people-reviews/page.tsx
@@ -1,17 +1,79 @@
 "use client";
 
+import { useState } from "react";
 import {
   CustomButtonComponent,
+  CustomModalComponent,
+  CustomTextAreaInputComponent,
+  CustomTextInputComponent,
   ReviewsAnalyticsComponent,
   ReviewsDisplayerComponent,
 } from "@/components";
-import { useRouter } from "next/navigation";
 
 const FeedbackPages = () => {
-  const router = useRouter();
+  const [showAddModal, setShowAddModal] = useState<boolean>(false);
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [currentRating, setCurrentRating] = useState<string>("");
+  const [review, setReview] = useState<string>("");
+  const [showSuccessModal, setShowSuccessModal] = useState<boolean>(false);
+
+  const [firstNameError, setFirstNameError] = useState<string>("");
+  const [lastNameError, setLastNameError] = useState<string>("");
+  const [currentRatingError, setCurrentRatingError] = useState<string>("");
+  const [reviewError, setReviewError] = useState<string>("");
 
   const handleWrite = () => {
-    router.push("/help/add-review");
+    setShowAddModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowAddModal(false);
+  };
+
+  const handleSubmit = () => {
+    let valid = true;
+
+    if (!firstName) {
+      setFirstNameError("First name is required");
+      valid = false;
+    } else {
+      setFirstNameError("");
+    }
+
+    if (!lastName) {
+      setLastNameError("Last name is required");
+      valid = false;
+    } else {
+      setLastNameError("");
+    }
+
+    if (!currentRating) {
+      setCurrentRatingError("Current rating is required and must be between 0 and 5");
+      valid = false;
+    } else {
+      setCurrentRatingError("");
+    }
+
+    if (!review) {
+      setReviewError("Review message is required");
+      valid = false;
+    } else {
+      setReviewError("");
+    }
+
+    if (!valid) return;
+
+    setShowAddModal(false);
+    setShowSuccessModal(true);
+    setTimeout(() => {
+      setShowSuccessModal(false);
+    }, 5000);
+
+    setFirstName("");
+    setLastName("");
+    setCurrentRating("");
+    setReview("");
   };
 
   return (
@@ -33,9 +95,123 @@ const FeedbackPages = () => {
 
       <div className="flex-grow border-t border-gray-400 my-6"></div>
 
-      <div className="md:my-6 lg:my-6 self-end">
+      <div className="flex flex-row justify-between md:my-6 lg:my-6">
+        <h2 className="text-white text-lg font-bold">Our customer reviews</h2>
         <CustomButtonComponent text="Leave a review" onClick={handleWrite} />
       </div>
+
+      {/* add review modal section */}
+      <CustomModalComponent show={showAddModal} onClose={handleCloseModal}>
+        <form
+          className="flex flex-col space-y-4"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <h2 className="text-xl text-white font-bold">
+            Write a review for us
+          </h2>
+
+          <div className="flex flex-row justify-between space-x-4">
+            <CustomTextInputComponent
+              type="text"
+              label={
+                !firstNameError ? (
+                  "First name"
+                ) : (
+                  <>
+                    {" "}
+                    {firstNameError && (
+                      <p className="text-red-500 text-[8px]">
+                        {firstNameError}
+                      </p>
+                    )}
+                  </>
+                )
+              }
+              placeholder="example"
+              value={firstName}
+              onChange={(text) => setFirstName(text)}
+            />
+
+            <CustomTextInputComponent
+              type="text"
+              label={
+                !lastNameError ? (
+                  "Last name"
+                ) : (
+                  <>
+                    {" "}
+                    {lastNameError && (
+                      <p className="text-red-500 text-[8px]">{lastNameError}</p>
+                    )}
+                  </>
+                )
+              }
+              placeholder="example"
+              value={lastName}
+              onChange={(text) => setLastName(text)}
+            />
+          </div>
+
+          <CustomTextInputComponent
+            type="number"
+            label={
+              !currentRatingError ? (
+                "Current rating"
+              ) : (
+                <>
+                  {" "}
+                  {currentRatingError && (
+                    <p className="text-red-500 text-[8px]">
+                      {currentRatingError}
+                    </p>
+                  )}
+                </>
+              )
+            }
+            placeholder="2"
+            value={currentRating}
+            onChange={(text) => setCurrentRating(text)}
+          />
+
+          <CustomTextAreaInputComponent
+            type="text"
+            label={
+              !reviewError ? (
+                "Review"
+              ) : (
+                <>
+                  {" "}
+                  {reviewError && (
+                    <p className="text-red-500 text-[8px]">{reviewError}</p>
+                  )}
+                </>
+              )
+            }
+            placeholder="Tell us about your experience..."
+            value={review}
+            onChange={(text) => setReview(text)}
+          />
+
+          <CustomButtonComponent text="Add review" onClick={handleSubmit} />
+        </form>
+      </CustomModalComponent>
+
+      {/* success modal section */}
+      <CustomModalComponent
+        show={showSuccessModal}
+        onClose={() => setShowSuccessModal(false)}
+      >
+        <div className="flex flex-col justify-center items-center text-white text-center">
+          <img
+            src="/images/web/confetti.webp"
+            alt="success-icon"
+            className="w-[80px] h-[80px] rounded-md self-center"
+          />
+
+          <h2 className="text-xl font-bold pt-4 pb-2">Success!</h2>
+          <p>Your review has been submitted successfully.</p>
+        </div>
+      </CustomModalComponent>
 
       <div className="mt-6">
         <ReviewsDisplayerComponent itemNumber={5} pagination />
